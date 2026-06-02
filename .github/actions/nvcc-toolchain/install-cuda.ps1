6dd6295a3530fd70035bd8a21a84d28ced3290ff
ptaylor <paul.e.taylor@me.com>
2026-06-02 21:38:06 +0000

    bump CUDA toolkit version in CI

--- .github/actions/nvcc-toolchain/install-cuda.ps1
+++ .github/actions/nvcc-toolchain/install-cuda.ps1
@@ -34,16 +34,36 @@ if ([version]$mmbVersionTag -le "11.5.0") {
     $cudaVersionUrl = "https://developer.download.nvidia.com/compute/cuda/${mmbVersionTag}/network_installers/cuda_${mmbVersionTag}_win10_network.exe"
 }
 
-$cudaComponents =
-    "nvcc_$mmVersionTag",
+# Keep the following list sorted.
+$cudaComponents = @(
+    "cudart_$mmVersionTag",
+    "cuobjdump_$mmVersionTag",
+    "cupti_$mmVersionTag",
     "curand_$mmVersionTag",
     "curand_dev_$mmVersionTag",
-    "cudart_$mmVersionTag",
-    "cupti_$mmVersionTag",
+    "cuxxfilt_$mmVersionTag",
+    "nvcc_$mmVersionTag",
+    "nvdisasm_$mmVersionTag",
+    "nvjitlink_$mmVersionTag",
+    "nvml_dev_$mmVersionTag",
     "nvrtc_$mmVersionTag",
     "nvrtc_dev_$mmVersionTag",
-    "nvml_dev_$mmVersionTag",
-    "nvtx_$mmVersionTag"
+    "nvtx_$mmVersionTag",
+    "visual_studio_integration_$mmVersionTag"
+)
+
+# nvfatbin first appeared as a separate VS component in 12.4.
+if ([int]$major -eq 12 -and [int]$minor -ge 4) {
+    $cudaComponents += "nvfatbin_$mmVersionTag"
+}
+
+# The following components first appeared in 13.0.
+if ([int]$major -ge 13) {
+    $cudaComponents += "crt_$mmVersionTag"
+    $cudaComponents += "nvfatbin_$mmVersionTag"
+    $cudaComponents += "nvvm_$mmVersionTag"
+    $cudaComponents += "nvptxcompiler_$mmVersionTag"
+}
 
 Invoke-WebRequest -Uri "$cudaVersionUrl" -OutFile "./cuda_network.exe" -UseBasicParsing
 Start-Process -Wait -PassThru -FilePath .\cuda_network.exe -ArgumentList "-s $cudaComponents"
